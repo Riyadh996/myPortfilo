e816bcc4004bcc4b24016fd7ee7444acf723547d
Khairul <riyadhrashid1996@gmail.com>
2023-10-12 23:06:07 +0200

    java add

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add JavaScript functionality here
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button element
+    var contactButton = document.getElementById('contact-button');
+
+    // Add a click event listener to the button
+    contactButton.addEventListener('click', function () {
+        alert('Button clicked! This is where your code would go.');
+    });
+});
